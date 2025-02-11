9a71f20e457f31dc505a42a6a010f9711b605976
choSH324 <12201304@inha.edu>
2025-02-11 10:36:46 +0900

    25021110

--- day01_02.py
+++ day01_02.py
@@ -4,6 +4,15 @@
 #     for i in range(1,10,1):
 #         print(f"{dan} x {i} = {dan*i}")
 
-dan = int(input("Input dan : "))
-for i in range(1,10,1):
-    print(f"{dan}*{i}={dan*i}")
+# for dan in range(2,10):
+#     for i in range(1,10,1):
+#         print(f"{dan} x {i} = {dan*i}")
+
+for dan in range(2,10): # 없이 나오돠 1씩 증가
+    for i in range(10): # 0부터 시작
+        print(f"{dan} x {i} = {dan*i}")
+
+# dan = int(input("Input dan : "))
+# for i in range(1,10,1):
+#     print(f"{dan}*{i}={dan*i}")
+
