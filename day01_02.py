7ca2d43397fa39216a15dbb04cb47c63883da704
choSH324 <12201304@inha.edu>
2025-02-11 10:33:47 +0900

    Version 25021110

--- day01_02.py
+++ day01_02.py
@@ -1,3 +1,9 @@
-for dan in range(2,10,1):
-    for i in range(1,10,1):
-        print(f"{dan} x {i} = {dan*i}")
+# 주석 처리 단축키 Ctrl + "/" (풀 때도 동일하게)
+
+# for dan in range(2,10,1):
+#     for i in range(1,10,1):
+#         print(f"{dan} x {i} = {dan*i}")
+
+dan = int(input("Input dan : "))
+for i in range(1,10,1):
+    print(f"{dan}*{i}={dan*i}")
