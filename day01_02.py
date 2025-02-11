e19c7d9525a5842bfc09fa89045e6cb5c6128f78
choSH324 <12201304@inha.edu>
2025-02-11 11:35:25 +0900

    Version 25021111

--- day01_02.py
+++ day01_02.py
@@ -52,7 +52,7 @@ n = int(input("Input Number : "))
 is_prime = True
 
 if n>=2:
-    for i in range(2,int(m.sqrt(n))+1,1):
+    for i in range(2,int(n**0.5)+1,1): # for i in range(2, ,int(m.sqrt(n))+1,1):
         print(i, end=' ')
         if n % i ==0:
             is_prime=False
