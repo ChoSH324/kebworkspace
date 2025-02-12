985e6ae11d63c97b12f7a217ce3d84aa91dbab85
choSH324 <12201304@inha.edu>
2025-02-12 17:43:45 +0900

    v 2.4

--- day03.py
+++ day03.py
@@ -20,19 +20,20 @@ drinks_foods_keys = list(drinks_foods)
 # print(drinks_foods_keys.remove("위스키"))
 # print(drinks_foods_keys)
 #print(random.choice(drinks_foods_keys))
+len=len(drinks_foods_list)
 while True:
     print("다음 술 중에 고르세요")
-    for i in range(1,len(drinks_foods_list)+1):
+    for i in range(1,len+1):
         print(f'{i}){drinks_foods_list[i-1][0]}', end = " ")
-    print(f'{len(drinks_foods_list)+1}) 아무거나',end = " ")
-    print(f'{len(drinks_foods_list)+2}) 종료 : ')
+    print(f'{len+1}) 아무거나',end = " ")
+    print(f'{len+2}) 종료 : ')
     menu = input()
     menu = int(menu)
-    if 1<=menu<=len(drinks_foods_list):
+    if 1<=menu<=len:
         print(f'{drinks_foods_list[menu-1][0]}에 어울리는 안주는 {drinks_foods_list[menu-1][1]} 입니다')
-    elif menu == len(drinks_foods_list)+1:
-        random_number=random.randint(0,len(drinks_foods_list)-1)
+    elif menu == len+1:
+        random_number=random.randint(0,len-1)
         print(f'{drinks_foods_list[random_number][0]}에 어울리는 안주는 {drinks_foods_list[random_number][1]} 입니다')
-    elif menu == len(drinks_foods_list)+2:
+    elif menu == len+2:
         print(f'다음에 또 오세요')
         break
