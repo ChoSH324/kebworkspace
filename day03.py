b04086e7d8374cc9ad633a08984ab1115d69f8e4
choSH324 <12201304@inha.edu>
2025-02-12 16:58:27 +0900

    v 2.4

--- day03.py
+++ day03.py
@@ -23,20 +23,12 @@ drinks_foods_keys = list(drinks_foods)
 
 while True:
     menu = input(f'다음 술중에 고르세요.\n1) {drinks_foods_list[0][0]}   2) {drinks_foods_list[1][0]}   3) {drinks_foods_list[2][0]}   4) {drinks_foods_list[3][0]}   5) {drinks_foods_list[4][0]}   6) 아무거나   7) 종료 : ')
-    if menu == '1':
-        print(f'{drinks_foods_list[0][0]}에 어울리는 안주는 {drinks_foods_list[0][1]} 입니다')
-    elif menu == '2':
-        print(f'{drinks_foods_list[1][0]}에 어울리는 안주는 {drinks_foods_list[1][1]} 입니다')
-    elif menu == '3':
-        print(f'{drinks_foods_list[2][0]}에 어울리는 안주는 {drinks_foods_list[2][1]} 입니다')
-    elif menu == '4':
-        print(f'{drinks_foods_list[3][0]}에 어울리는 안주는 {drinks_foods_list[3][1]} 입니다')
-    elif menu == '5':
-        print(f'{drinks_foods_list[4][0]}에 어울리는 안주는 {drinks_foods_list[4][1]} 입니다')
-    elif menu == '6':
+    menu = int(menu)
+    if 1<menu<6:
+        print(f'{drinks_foods_list[menu-1][0]}에 어울리는 안주는 {drinks_foods_list[menu-1][1]} 입니다')
+    elif menu == 6:
         random_number=random.randint(0,5)
-
         print(f'{drinks_foods_list[random_number][0]}에 어울리는 안주는 {drinks_foods_list[random_number][1]} 입니다')
-    elif menu == '7':
+    elif menu == 7:
         print(f'다음에 또 오세요')
         break
