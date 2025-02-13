29e28301c51fcb6630e488f02ea09acca4b92f72
choSH324 <12201304@inha.edu>
2025-02-13 16:14:48 +0900

    v3.6

--- day04.py
+++ day04.py
@@ -1,11 +1,48 @@
-# SOLID
-# Open Closed Principle : 개방 폐쇄 원칙 (확장에는 열려 있고 수정에는 닫혀 있는 원칙)
-def factorial_repetition(n) -> int:
-    result=1
-    for _ in range(2,n+1):
-        result = result*_
-    return result
+import time
+# decorator
+def description(f):  # closure
+    def inner(*args):
+        print(f.__name__)
+        print(f.__doc__)
+        r = f(*args)
+        return r
+
+    return inner
 
 
-number = int(input())
-print(f"{number}! = {factorial_repetition(number)}")
+def time_decorator(func):
+    def wrapper(*arg):
+        s = time.time()
+        r = func(*arg)
+        e = time.time()
+        print(f'실행시간 : {e - s}초')
+        return r
+    return wrapper
+
+#@time_decorator
+#@description
+def squares(n):
+    """
+    제곱 함수
+    """
+    return n * n
+
+
+
+f1 = squares(2)
+f2 = description(time_decorator(squares))
+print(f1)
+print(f2(3))
+# squares(4)가 먼저 진행하여 time_decorator의 인자로 16을 주므로 의도되로 진행X
+print(description(time_decorator(squares(4))))
+# 2중 데코레이터 적용 가장 쉬운 방법
+@time_decorator
+@description  # 데코레이터를 쓰는 순서로도 차이를 보인다
+# description 함수 호출에 대한 걸 마지막에 씌워야 한다
+def squares1(n):
+    """
+    제곱 함수
+    """
+    return n * n
+
+print(squares1(5))
