377b36a907c05014a77f13a702624c5e9d4a741a
choSH324 <12201304@inha.edu>
2025-02-17 11:41:19 +0900

    v6.4

--- day06.py
+++ day06.py
@@ -1,3 +1,4 @@
+
 def total_sum(a)->int:
     """
 
@@ -9,3 +10,12 @@ def total_sum(a)->int:
     return a + total_sum( a-1 )
 a=int(input())
 print(total_sum(a))
+
+n = int(input())
+print(n*(n+1)/2)
+
+n2=int(input())
+total=0
+for i in range(1,n2+1):
+    total+=a
+print(total)
