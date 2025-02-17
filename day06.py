3f771340c9d9b06b757af25f192c405432395c2e
choSH324 <12201304@inha.edu>
2025-02-17 10:40:21 +0900

    v6.4

--- day06.py
+++ day06.py
@@ -1,15 +1,7 @@
-def is_even(n) -> bool:
-    """
-    짝수 판정 함수
-    :param n: int
-    :return: 짝수면 True, 홀수면 False
-    """
-    return not n&1
-a=int(input())
-print(is_even(a))
-
-# a=10 # 0000 1010
-# b=11 # 0000 1011
-# print(a & b) # AND 연산 --> 0000 1010
-# print(a | b) # OR 연산 --> 0000 1011
-# print(a ^ b) # BETA 연산 --> 0000 0001
+def dec_oct(n) ->int:
+    if n==0:
+        return ""
+    else :
+        return dec_oct(n // 8) + str(n % 8)
+n = int(input())
+print(dec_oct(n))
