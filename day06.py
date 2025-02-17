436b94795bbb3368365fb61a67889698b5f33cf9
choSH324 <12201304@inha.edu>
2025-02-17 09:46:34 +0900

    v6.2

--- day06.py
+++ day06.py
@@ -4,10 +4,14 @@ def is_even(n) -> bool:
     :param n: int
     :return: 짝수면 True, 홀수면 False
     """
-    if n%2==0:
+    if n&1==0:
         return True
-    else: return False
-
-
+    return False
 n=int(input())
 print(is_even(n))
+
+# a=10 # 0000 1010
+# b=11 # 0000 1011
+# print(a & b) # AND 연산 --> 0000 1010
+# print(a | b) # OR 연산 --> 0000 1011
+# print(a ^ b) # BETA 연산 --> 0000 0001
