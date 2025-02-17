f9e5e7688b2f840c3479c2692d0fa1c88bcf773e
choSH324 <12201304@inha.edu>
2025-02-17 10:07:46 +0900

    v.6.3

--- day02.py
+++ day02.py
@@ -1,24 +1,61 @@
+# Assignment Day 02
+# v1.4) Make my_pow custom function instead of ** operator, power function and make it work.
+import math
 
-# Last Assignment
-def my_pow(a,b):
-    return a**b
 
-def is_prime(num) -> bool: # -> bool : type hint 라고 한다 반환 자료 타입을 명시해주는 것
+def my_pow(b, e) -> float:
     """
-    A function that returns Ture if it is a prime number and False if it is not a prime number
-    : param num : integer number
-    : return : boolean type
+    A user-defined function that receives a base and exponent and returns the power result in the form of a real number
+    :param b: base number
+    :param e: exponent
+    :return: the power result in the form of a real number
     """
-    if num>=2:
-         for i in range(2,int(my_pow(num,0.5))+1,1):
-             if num % i == 0:
+    if e < 0:
+        b = 1 / b
+        e = e * -1
+
+    result = 1
+
+    i = int(e)
+    f = e - i
+
+    for _ in range(i):  # for k in range(e):
+        result = result * b
+
+    if f > 0:
+        result = result * math.exp(f * math.log(b))
+
+    return result
+
+
+def is_prime(num) -> bool:
+    """
+    A function that returns True if it is a prime number and False if it is not a prime number
+    :param num: integer number
+    :return: boolean type
+    """
+    if num >= 2:
+        i = 2
+        #while i < (int(my_pow(num, 0.5)) + 1):
+        while i*i < num+1:
+            if num % i == 0:
                 return False
+            i = i + 1
     else:
         return False
     return True
-# main
-n = int(input("Input Number : "))
-if is_prime(n):
-    print(f"{n} is prime number")
-else:
-    print(f"{n} is NOT prime number")
+
+
+#print(my_pow(2, 9))
+numbers = input("Input number : ").split()  # ex) 900 1000
+n1 = int(numbers[0])
+n2 = int(numbers[1])
+
+if n1 > n2:
+    n1, n2 = n2, n1
+
+j = n1
+while j <= n2:
+    if is_prime(j):
+        print(j, end=' ')
+    j = j + 1
