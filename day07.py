268f5793c9ae670ae27d9ac24b87809534ae4d35
choSH324 <12201304@inha.edu>
2025-02-18 10:07:05 +0900

    v7.1

--- day07.py
+++ day07.py
@@ -1,26 +1,17 @@
-class Stack:
-    def __init__(self):
-        self.items=list()
-
-    def push(self,data):
-        self.items.append(data)
-
-    def pop(self):
-        return self.items.pop()
-
-    def size(self):
-        return len(self.items)
-
-    def peek(self):
-        return self.items[-1]
+def a(i):
+    j=9
+    print(i,j)
 
 
-s1 = Stack()
-s1.push(1)
-s1.push(2)
-s1.push(11)
-print(s1.peek())
-print(s1.size())
-s1.pop()
-print(s1.peek())
-print(s1.size())
+def b(k):
+    a(1)
+    print(k)
+
+
+def main():
+    print("start")
+    b(3)
+
+
+if __name__ == "__main__":
+    main()
