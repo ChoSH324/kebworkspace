05c2f5dbf366f92e47d768057d991cb6ef1eb39a
choSH324 <12201304@inha.edu>
2025-02-18 15:31:52 +0900

    v7.4

--- day07.py
+++ day07.py
@@ -1,9 +1,46 @@
-from collections import deque
+class Node:
+    def __init__(self,data,next=None):
+        self.data=data
+        self.next=next
 
-d=deque()
-d.append(7)
-d.append(-11)
-d.append(8)
 
-if __name__=="__main__":
-    print(d)
+class Queue:
+    def __init__(self):
+        self.front=None
+        self.rear = None
+        self._size=0
+
+
+    def enqueue(self,item):
+        self._size+=1
+        node = Node(item)
+        if self.rear is None:
+            self.front = node
+            self.rear = node
+        else:
+            self.rear.next = node
+            self.rear = node
+
+
+    def dequeue(self):
+        if self.front is None:
+            raise IndexError('pop from empty queue')
+        self._size -= 1
+        temp=self.front
+        self.front=self.front.next
+        if self.front is None:
+            self.rear = None
+        return temp.data
+
+
+    def size(self):
+        return self._size
+
+
+if __name__ == "__main__":
+    a = Queue()
+    a.enqueue(5)
+    a.enqueue(7)
+    a.enqueue(1)
+    print(a.size())
+    print(a.dequeue())
