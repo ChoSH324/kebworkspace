7f055cd4451927422f8697971bc5aa35652f7ec2
choSH324 <12201304@inha.edu>
2025-02-18 11:18:27 +0900

    v7.2

--- day07.py
+++ day07.py
@@ -1,17 +1,34 @@
-def a(i):
-    j=9
-    print(i,j)
+class Node:
+    def __init__(self,data,next=None):
+        self.data=data
+        self.next=next
 
+class LinkdedList:
+    def __init__(self):
+        self.head = None
 
-def b(k):
-    a(1)
-    print(k)
+    def append(self,data):
+        if not self.head:
+            self.head=Node(data)
+            return
+        current = self.head
+        while current.next:
+            current=current.next
+        current.next=Node(data)
+        if not self.head:
+            self.head=Node(data)
+            return
 
-
-def main():
-    print("start")
-    b(3)
-
+    def __str__(self):
+        node = self.head
+        while node is not None:
+            print(node.data)
+            node=node.next
+        return print("end")
 
 if __name__ == "__main__":
-    main()
+    l = LinkdedList()
+    l.append(7)
+    l.append(-11)
+    l.append(4)
+    l.__str__()
