17729376ca90f27695687c6bdd6dd091a9e4ac71
choSH324 <12201304@inha.edu>
2025-02-19 15:13:27 +0900

    v8.4

--- day08.py
+++ day08.py
@@ -1,77 +1,18 @@
-class TreeNode:
-	def __init__(self):
-		self.left = None
-		self.data = None
-		self.right = None
+def dfs(g, v, visited):
+    visited[v] = True
+    print(chr(ord('A')+v), end=' ')
+    for i in range(len(g)):
+        if g[v][i] == 1 and not visited[i]:
+            dfs(g, i, visited)
 
 
-if __name__ == "__main__":
-    numbers = [10, 15, 8, 3, 9, 6, 2, 16]
+graph = [
+    [0, 0, 1, 1, 0],
+    [0, 0, 1, 0, 0],
+    [1, 1, 0, 1, 1],
+    [1, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0]
+]
 
-    node = TreeNode()
-    node.data = numbers[0]
-    root = node
-
-    for group in numbers[1:]:
-        node = TreeNode()
-        node.data = group
-        current = root
-        while True:
-            if group < current.data:
-                if current.left is None:
-                    current.left = node
-                    break
-                current = current.left  # move
-            else:
-                if current.right is None:
-                    current.right = node
-                    break
-                current = current.right  # move
-
-    print("BST 구성 완료")
-
-    find_group = int(input())
-
-    current = root
-    while True:
-        if find_group == current.data:
-            print(f"{find_group}을(를) 찾았습니다")
-            break
-        elif find_group < current.data:
-            if current.left is None:
-                print(f"{find_group}이(가) 존재하지 않습니다")
-                break
-            current = current.left
-        else:
-            if current.right is None:
-                print(f"{find_group}이(가) 존재하지 않습니다")
-                break
-            current = current.right
-
-    parent = None
-    current = root
-
-
-def delete(node,delete_data):
-    parent = None
-    current = root
-    # 루트에서 없어지지 않는다면
-    if current.left is None and current.right is None:
-       if parent.right == current.data:
-             parent.right = None
-          else:
-             parent.left = None
-
-        # 자식 노드가 왼쪽에만 있는 경우
-    elif current.left is not None and current.right is None:
-        if parent.right == current.data:
-            parent.right = current.left
-          else:
-               parent.left = current.left
-    elif current.left is None and current.right is not None:
-        if parent.right == current.data:
-              parent.right = current.right
-        else:
-            parent.left = current.right
-    else:
-
+visited = [0] * len(graph)
+dfs(graph, 0, visited)
