c7b1c172263ff46890ae3ec71f16c6b5c2182c21
choSH324 <12201304@inha.edu>
2025-02-21 13:20:53 +0900

    v10 time_bubble_insert_sort"
    q
    quit

--- day10.py
+++ day10.py
@@ -1,11 +1,47 @@
-def insertion_sort(a_list):
-    for i in range(1,len(a_list)):
-        value = a_list[i]
-        while i > 0 and a_list[i-1] > value:
-            a_list[i] = a_list[i-1]
-            i = i-1
-        a_list[i]=value
-    return a_list
+import time
+import random
 
-a= [6, 5, 2, 8]
-print(insertion_sort(a))
+def time_decorator(func):
+    def wrapper(*arg):
+        s = time.time()
+        r = func(*arg)
+        e = time.time()
+        print(f'실행시간 : {e - s}초')
+        return r
+    return wrapper
+
+
+@time_decorator
+def insertion_sort(l):
+    for i in range(1, len(l)):
+        value = l[i]
+        while i > 0 and l[i-1] > value:
+            l[i] = l[i-1]
+            i = i - 1
+            #print(i, end=' ')
+        l[i] = value
+    return l
+
+
+@time_decorator
+def bubble_sort(l):
+    for i in range(len(l) - 1):
+        no_swap = True
+        for j in range(len(l) - 1 - i):
+            if l[j] > l[j+1]:
+                l[j], l[j + 1] = l[j+1], l[j]
+                no_swap = False
+                #print(j, end=' ')
+        if no_swap:
+            return l
+    return l
+
+
+
+lists1 = [random.randint(1, 100000) for _ in range(10000)]
+lists2 = lists1.copy()
+print("bubble_sort")
+bubble_sort(lists1)
+print("insertion_sort")
+insertion_sort(lists2)
+
